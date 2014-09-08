c4c74f578755d38a7ae5253b3b5ec4bb5813442c
Klaus Alfert <klaus.alfert@googlemail.com>
2014-09-08 08:35:22 +0200

    Test some simple options

--- test/coverex_test.exs
+++ test/coverex_test.exs
@@ -1,7 +1,21 @@
 defmodule CoverexTest do
-  use ExUnit.Case
+	use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+	alias Coverex.Task
+	
+	test "coveralls wished?" do
+	  	opts = [coveralls: true]
+	  	assert Task.post_to_coveralls?(opts) 
+	end
+
+	test "coveralls not wished?" do
+	  	opts = [coveralls: false]
+	  	refute Task.post_to_coveralls?(opts) 
+	end
+
+	test "is coveralls requested on the commandline?" do
+		conf = Mix.Project.config()
+		IO.inspect conf
+		assert Task.post_to_coveralls?(conf[:test_coverage])
+	end
 end
