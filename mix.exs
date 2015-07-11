41d2141d82c55faffe4bb8cee0fb373ab457c95a
Klaus Alfert <klaus.alfert@googlemail.com>
2015-07-11 16:38:31 +0200

    bump version to 1.4.2-dev

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Coverex.Mixfile do
 
   def project do
     [app: :coverex,
-     version: "1.4.1",
+     version: "1.4.2-dev",
      elixir: "~> 1.0.0",
      package: package,
      name: "Coverex - Coverage Reports for Elixir",
