5e6522b431fb358e6a13d3956634608a62f96fbc
Klaus Alfert <klaus.alfert@googlemail.com>
2015-08-21 19:36:17 +0200

    Updating dependencies

--- mix.exs
+++ mix.exs
@@ -31,11 +31,11 @@ defmodule Coverex.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:httpoison, "0.7.0"},
+      {:httpoison, "0.7.2"},
       {:poison, "~> 1.4.0"},
       {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.7.2", only: :dev},
-      {:dialyze, "~> 0.1.4", only: :dev}
+      {:ex_doc, "~> 0.8.4", only: :dev},
+      {:dialyze, "~> 0.2.0", only: :dev}
     ]
   end
 
