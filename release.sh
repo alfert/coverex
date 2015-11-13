a48f5901e5f440833c025544bab33403b1fc0998
Klaus Alfert <klaus.alfert@gmail.com>
2015-11-13 16:52:15 +0100

    prepare for release 1.4.8

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@
 set -x
 
 # old is always with -dev
-old="1.4.6"
-new="1.4.7-dev"
+old="1.4.7"
+new="1.4.8-dev"
 # do not set any variables beyond this line
 old_version="$old-dev"
 release_version="$old"
