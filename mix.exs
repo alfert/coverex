699934e7d006c78dffc7539729e79a67698d02c6
Klaus Alfert <klaus.alfert@gmail.com>
2015-11-17 21:27:08 +0100

    Example configuration

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Coverex.Mixfile do
      homepage_url: "https://github.com/alfert/coverex",
      docs: [readme: "README.md"],
      description: description,
-     test_coverage: [tool: Coverex.Task, coveralls: true],
+     test_coverage: [tool: Coverex.Task, coveralls: true, ignore_modules: []],
      deps: deps]
   end
 
