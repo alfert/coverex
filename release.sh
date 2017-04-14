10eae58803831c424f512af572d6b64fb3f78cc3
Klaus Alfert <klaus.alfert@gmail.com>
2017-04-14 10:41:06 +0200

    more comments guiding the user

--- release.sh
+++ release.sh
@@ -30,8 +30,10 @@ git commit -m "bump version to $release_version" mix.exs
 
 # tag the commit
 git tag -a -m "new release version v$release_version" v$release_version
+echo "updated mix file is committed and tagged with v$release_version"
 
 # Upload to Hex.PM (both package and docs) 
+echo "Publish now to Hex"
 mix hex.publish
 
 # update version in mix.exs
@@ -39,6 +41,8 @@ sed -i $ORIG "s/\(version: \"\)$release_version\",/\\1$new_version\",/" mix.exs
 
 # add to git
 git commit -m "bump version to $new_version" mix.exs
+echo "updated mix file is committed with version $new_version"
 
 # push to github
+echo "Now pushing all changes to GitHub"
 git push origin master --tags
